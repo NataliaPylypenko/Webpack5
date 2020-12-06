31b2560854aaa5576ae177bea07a4a82fe3ac290
Natalia <natalia28ivanivna@gmail.com>
2020-12-06 19:28:17 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import example from './images/example.png',
+import example from './images/example.svg',
+import './styles/main.css',
+
+// создание свойства класса без конструктора
+class Game {
+    name = 'Violin Charades'
+}
+const myGame = new Game()
+
+// создаем параграф
+const p = document.createElement('p')
+p.textContent = `I like ${myGame.game}.`
+
+// создаем элемент заголовка
+const heading = document.createElement('h1')
+heading.textContent = 'Как интересно!'
+
+// добавляем параграф и заголовок в DOM
+const root = document.querySelector('#root')
+root.append(heading, p)
